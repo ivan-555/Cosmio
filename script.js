abf227e2c7d2920875f4f0bac381fe14f716ea13
Ivan Facchinetti <ivan.facchinetti55@gmail.com>
2024-10-11 02:28:09 +0200

    Base Version

---
+++ script.js
@@ -0,0 +1,115 @@
+const NavLinks = document.querySelectorAll('aside nav ul li a');
+const NavIcon = document.querySelector('aside nav ul .icon');
+const preloader = document.querySelector('.preloader');
+const pages = document.querySelectorAll('.page');
+const home = document.querySelector('.home');
+const sidebar = document.querySelector('#sidebar');
+const sidebarHeader = document.querySelector('#sidebar header');
+const hamburger = document.querySelector('.hamburger');
+const viewInfoButtons = document.querySelectorAll('.viewInfo');
+const exploreButtons = document.querySelectorAll('.explore');
+const preloaderTexts = document.querySelectorAll('.preloader .text');
+const preloaderSplineObject = document.querySelector('.preloader #preloaderSplineObject');
+const prePreloader = document.querySelector('.preloader .pre-preloader');
+const preloaderLoadingText = document.querySelector('.preloader .loading-text');
+const audioPlayer = document.querySelector('#audioPlayer');
+const audioSlider = document.querySelector('.audio-slider');
+const audioSliderCircle = document.querySelector('.audio-slider .circle');
+const audioSliderMuteIcon = document.querySelector('.audio-slider .fa-volume-xmark');
+const audioSliderUnmuteIcon = document.querySelector('.audio-slider .fa-volume-high');
+
+//Page Preloader
+window.addEventListener('load', () => {
+    setTimeout(() => {
+        preloader.classList.add('hide');
+    }, 8000);
+});
+// Zeige den Text nach einer Verzögerung an, die das Laden des Spline-Objekts berücksichtigt
+setTimeout(() => {
+  preloaderTexts.forEach(preloaderText => {
+    preloaderText.style.opacity = "1"; // Text anzeigen
+  });
+
+  prePreloader.style.opacity = "0"; // Pre-Preloader ausblenden
+  preloaderSplineObject.style.opacity = "1"; // Spline-Objekt anzeigen
+  preloaderLoadingText.style.animation = "text-fading 3s infinite"; // Textanimation starten
+}, 3000);
+
+
+// Scrollen per Mausrad deaktivieren
+window.addEventListener("wheel", function(e) {
+    e.preventDefault(); // Verhindert das Scrollen
+  }, { passive: false }); // 'passive: false' erlaubt die Nutzung von e.preventDefault()
+
+ // Verhindert das Scrollen bei gedrücktem Mausrad
+ document.addEventListener('mousedown', function(event) {
+  // Überprüfen, ob das mittlere Mausrad gedrückt wird (event.button === 1)
+  if (event.button === 1) {
+      event.preventDefault();  // Standardaktion wie Scrollen verhindern
+  }
+});
+
+// Sidebar Toggle
+hamburger.addEventListener('click', () => {
+  sidebar.classList.toggle('show');
+  hamburger.classList.toggle('isX');
+});
+
+// Navigate Between Pages
+// NavLinks Icon Movement
+NavLinks.forEach((link, index) => {
+  link.addEventListener('click', () => {
+    home.style.display = 'none';
+    pages.forEach(page => page.classList.remove('active'));
+    pages[index].classList.add('active');
+    NavIcon.style.transform = `translateY(${index * 50}px) rotate(90deg)`;
+    NavIcon.classList.add('active');
+  });
+});
+
+sidebarHeader.addEventListener('click', () => {
+  home.style.display = 'block';
+  pages.forEach(page => page.classList.remove('active'));
+  NavIcon.classList.remove('active');
+  setTimeout(() => {
+    NavIcon.style.transform = `translateY(0px) rotate(90deg)`;
+  }, 200);
+});
+
+//View Info and Explore Buttons
+viewInfoButtons.forEach(viewInfoButton => {
+  viewInfoButton.addEventListener('click', () => {
+    window.scrollTo({
+      top: window.innerHeight, // window.innerHeight = 100vh
+      behavior: 'smooth'
+    });
+  });
+});
+
+exploreButtons.forEach(exploreButton => {
+  exploreButton.addEventListener('click', () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  });
+});
+
+// Music Mute Slider
+let isMuted = true;
+
+audioSlider.addEventListener('click', () => {
+  isMuted = !isMuted;
+  if (isMuted) {
+    audioSliderCircle.style.left = "0";
+    audioSliderMuteIcon.classList.add('active');
+    audioSliderUnmuteIcon.classList.remove('active');
+    audioPlayer.muted = true;  // Audio stummschalten
+    
+  } else {
+    audioSliderCircle.style.left = "30px";
+    audioSliderMuteIcon.classList.remove('active');
+    audioSliderUnmuteIcon.classList.add('active');
+    audioPlayer.muted = false;  // Audio wieder unmuten
+  }
+});
