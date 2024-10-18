a764b7f597ec691be3ea0108bccde2a77e4d8855
Ivan Facchinetti <ivan.facchinetti55@gmail.com>
2024-10-18 03:05:58 +0200

    code cleanup

--- script.js
+++ script.js
@@ -13,11 +13,11 @@ const MWLoaders = document.querySelectorAll('.MWLoader');
 const pageInfos = document.querySelectorAll('.page .info');
 const homeInfo = document.querySelector('.home .info');
 
-//Page Preloader
+// Hide Page Preloader
 window.addEventListener('load', () => {
     setTimeout(() => {
         preloader.classList.add('hide');
-    }, 6000);
+    }, 6000); // 6s preloader while models are loading
 });
 
  // Verhindert das Scrollen bei gedrücktem Mausrad
@@ -61,56 +61,57 @@ hamburger.addEventListener('click', () => {
   }
 });
 
+// Standard Close Sidebar on Mobile
 if (window.innerWidth < 1450) {
   sidebar.classList.remove('show');
   hamburger.classList.remove('isX');
 }
 
 // Navigate Between Pages
-// NavLinks Icon Movement
 NavLinks.forEach((link, index) => {
   link.addEventListener('click', () => {
     home.style.display = 'none';
     pages.forEach(page => page.classList.remove('active'));
     pages[index].classList.add('active');
-    NavIcon.style.transform = `translateY(${index * 50}px) rotate(90deg)`;
-    NavIcon.classList.add('active');
+    NavIcon.style.transform = `translateY(${index * 50}px) rotate(90deg)`; // 50px = 1 NavLink height * index of the clicked NavLink moves the Rocket Icon to the clicked NavLink
+    NavIcon.classList.add('active'); // Show Rocket Icon if we are not on the home page
     if (window.innerWidth < 1450) {
-      sidebar.classList.remove('show');
+      sidebar.classList.remove('show'); // Close Sidebar on Mobile if a NavLink is clicked
       hamburger.classList.remove('isX');
     }
   });
 });
-
+// Header click brings you back to the home page
 sidebarHeader.addEventListener('click', () => {
   home.style.display = 'block';
   pages.forEach(page => page.classList.remove('active'));
-  NavIcon.classList.remove('active');
+  NavIcon.classList.remove('active'); // Hide Rocket Icon if we are on the home page
   setTimeout(() => {
     NavIcon.style.transform = `translateY(0px) rotate(90deg)`;
-  }, 200);
+  }, 200); // 200ms delay to move the Rocket Icon back up to the home NavLink
   if (window.innerWidth < 1450) {
-    sidebar.classList.remove('show');
+    sidebar.classList.remove('show'); // Close Sidebar on Mobile if a NavLink is clicked
     hamburger.classList.remove('isX');
   }
 });
 
 //View Info and Explore Buttons
 viewInfoButtons.forEach(viewInfoButton => {
+  // Moves the page down to the info when a view info button is clicked
   viewInfoButton.addEventListener('click', () => {
     window.scrollTo({
       top: window.innerHeight, // window.innerHeight = 100vh
       behavior: 'smooth'
     });
     if (window.innerWidth < 1450) {
-      sidebar.classList.remove('show');
+      sidebar.classList.remove('show'); // Close Sidebar on Mobile if a view info button is clicked
       hamburger.classList.remove('isX');
-      homeInfo.classList.remove('blur');
-      pageInfos.forEach(pageInfo => pageInfo.classList.remove('blur'));
+      homeInfo.classList.remove('blur'); // Remove blur effect on home info
+      pageInfos.forEach(pageInfo => pageInfo.classList.remove('blur')); // Remove blur effect on page infos
     }
   });
 });
-
+// Moves the page up to the model viewer when the explore button is clicked
 exploreButtons.forEach(exploreButton => {
   exploreButton.addEventListener('click', () => {
     window.scrollTo({
@@ -124,7 +125,7 @@ exploreButtons.forEach(exploreButton => {
 modelViewers.forEach((modelViewer, index) => {
   modelViewer.addEventListener('load', () => {
     setTimeout(() => {
-      MWLoaders[index].style.display = 'none';
+      MWLoaders[index].style.display = 'none'; // Hide the loader when the model is loaded
     }, 1300); // 1200ms Verzögerung nachdem das modell geladen wurde da es sonst flasht
   });
 });
